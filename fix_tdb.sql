79f71b46e6137ff6c7409642f528c0b1e063182f
privatecore <rial9111@rambler.ru>
2012-05-05 19:16:25 +0500

    - Думаю фикс TDB
    Signed-off-by: privatecore <rial9111@rambler.ru>

---
+++ fix_tdb.sql
@@ -0,0 +1,4 @@
+UPDATE creature_template SET faction_A = '1802', faction_H = '1802' WHERE entry IN (30499,28312,28319);
+UPDATE creature_template SET faction_A = '1801', faction_H = '1801' WHERE entry IN (30400,32629,32627);
+UPDATE creature_template SET npcflag=npcflag|32768 WHERE entry   IN (31841,31842);
+UPDATE creature_template SET npcflag=npcflag|32768 WHERE entry IN (31841,31842);
